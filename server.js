d243c1061874411497044bb231fc72386a6d594a
Brent Doering <brentiscoding@gmail.com>
2023-06-02 00:44:23 -0600

    Finished backend REST API and db setup

--- server.js
+++ server.js
@@ -1,50 +1,21 @@
 const express = require('express');
-const port = 3000;
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+
+connectDB();
 
 const app = express();
 
-const ideas = [
-    {
-      id: 1,
-      text: 'Give Sherwood his meds',
-      tag: 'Child',
-      username: 'BrentMan',
-      date: '2023-05-07',
-    },
-    {
-      id: 2,
-      text: 'Buy More Oat Milk',
-      tag: 'Shopping',
-      username: 'NickMan',
-      date: '2023-05-15',
-    },
-    {
-      id: 3,
-      text: 'Figure out when Casa Bonita opens and make reservations',
-      tag: 'Life',
-      username: 'BrentMan',
-      date: '2023-05-12',
-    },
-  ];
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World' });
 });
 
-// Get all ideas
-app.get('/api/ideas', (req, res) => {
-    res.json({ success: 'true', data: ideas });
-});
-
-// Get all ideas
-app.get('/api/ideas/:id', (req, res) => {
-    const idea = ideas.find((idea) => idea.id === +req.params.id);
-
-    if (!idea) {
-       return res.status(404).json({ success: 'false', error: 'Resource not found'});
-    }
-
-    res.json({ success: 'true', data: idea });
-});
+const ideasRouter = require('./routes/ideas');
+app.use('/api/ideas', ideasRouter)
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
